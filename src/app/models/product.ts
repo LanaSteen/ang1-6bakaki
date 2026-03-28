b252990f354897604a4fb942126cb9ab9d7ad3d6
LanaNew <l4n.steen@gmail.com>
2026-03-28 18:46:51 +0400

    3

---
+++ src/app/models/product.ts
@@ -0,0 +1,25 @@
+
+
+
+class Product {
+   id! : number;
+   name? : string;
+   price? : number;
+   nuts? : boolean;
+   image? : string;
+   vegeterian? : boolean;
+   spiciness? : number;
+   categoryId? : number;
+}
+
+
+
+
+// "id": 2,
+//     "name": "Som tam papaya salad",
+//     "price": 9.5,
+//     "nuts": false,
+//     "image": "https://course-jsbasic.javascript.ru/assets/products/som_tam_papaya_salad.png",
+//     "vegeterian": true,
+//     "spiciness": 0,
+//     "categoryId": 1
